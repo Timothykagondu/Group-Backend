161deb3e7f70fa0db6839212cb76d7063ce1fde0
Rukorio-Vanessa <blwwbl55@gmail.com>
2023-03-29 01:29:27 +0300

    added validations for movie

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,8 @@
 class Movie < ApplicationRecord
+    validates :title, presence: true
+
+    validates :year, numericality: {greater_than_or_equal_to: 1888,
+                                    less_than_or_equal_to: Date.today.year
+                                   }
+    validates :image_url, presence: true
 end
