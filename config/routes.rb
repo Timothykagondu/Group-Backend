309b3c3351596d8b5e42f88ae1cc27b0b5a73423
+ <+>
2023-03-28 21:25:47 +0300

    created migrations for reviews

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  resources :reviews
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
   resources :users, eonly: [:index, :show, :create]
 end
+# rails generate model Review user:references movie:references rating:integer comment:text
+# rails db:migrate
+# rails generate resource Review user:references movie:references rating:integer comment:text
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_28_123531) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_28_181617) do
+  create_table "reviews", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "movie_id"
+    t.text "comment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
