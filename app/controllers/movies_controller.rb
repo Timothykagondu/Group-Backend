0498ecca6897bb2b21ce458504ff0a042e8a20c3
Rukorio-Vanessa <blwwbl55@gmail.com>
2023-03-29 02:19:11 +0300

    created GET method to dispaly a specific movie

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,21 @@
 class MoviesController < ApplicationController
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+
     def index
         render json: Movie.all, status: :ok
+    end
+
+    def show
+        movie = find_movie
+        render json: movie, status: :ok
+    end
+
+    private
+    def find_movie
+        Movie.find(params[:id])
+    end
+
+    def render_not_found_response
+        render json: { error: "Movie not found" }, status: :not_found
     end
 end
