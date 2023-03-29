160e5d300e6699a010e40f2ede3ae6e03a256bcd
Rukorio-Vanessa <blwwbl55@gmail.com>
2023-03-29 10:44:50 +0300

    created #create action for movie-genre

--- app/controllers/movie_genres_controller.rb
+++ app/controllers/movie_genres_controller.rb
@@ -1,3 +1,10 @@
 class MovieGenresController < ApplicationController
-    
+    def create
+        movie-genre = MovieGenre.create(movie_params) 
+        if movie-genre
+            response_template(status: :created, data: movie-genre)
+        else
+            response_template(status: :unprocessable_entity, data: movie-genre.errors, message: 'failed')
+        end
+    end
 end
