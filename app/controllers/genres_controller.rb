01b72edadd7b89cd16bb0990ac12e927a4f62061
Rukorio-Vanessa <blwwbl55@gmail.com>
2023-03-29 09:05:19 +0300

    created #show action for genre

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,5 +1,21 @@
 class GenresController < ApplicationController
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+
     def index
         render json: Genre.all, status: :ok
+    end
+
+    def show
+        genre = find_genre
+        render json: genre, status: :ok
+    end
+
+    private
+    def find_genre
+        Genre.find(params[:id])
+    end
+
+    def render_not_found_response
+        render json: { error: "Genre not found" }, status: :not_found
     end
 end
