9add1483f3b7ca4594bfc307a961e7c536be6258
+ <+>
2023-03-29 15:17:31 +0300

    created signup action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,22 @@
 class ApplicationController < ActionController::Base
     include ActionController::Cookies
+    rescue_from StandardError, with: :standard_error
+
+    def response_template(message: 'success', status: 200, data: nil)
+        render json: {message: message, data: data}, status: status
+    end
+
+    def save_user(id)
+        session[:uid] = id
+        session[:expiry] = 8.hours.from_now
+    end
+
+    def user
+        User.find(@uid) 
+    end
+
+    def standard_error(exception)
+        app_response(message: 'failed', data: { info: exception.message }, status: :unprocessable_entity)
+    end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,9 @@ class User < ApplicationRecord
     has_many :reviews
     has_many :sessions
     has_secure_password
+
+    validates :username, {presence: true, uniqueness: true, length: { minimum: 3, maximum: 12 }}
+    validates :email, {presence: true, uniqueness: true}
+    validates :password, presence: true
+    
 end
