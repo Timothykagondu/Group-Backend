25ec3cbb88481ab024601c940b6c6cc8b9651b96
Rukorio-Vanessa <blwwbl55@gmail.com>
2023-03-30 07:22:53 +0300

    defined login action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,17 @@ class UsersController < ApplicationController
         end
     end
 
+    def login
+        sql = "username = :username OR email = :email"
+        user = User.where(sql, { name: user_params[:name], email: user_params[:email] }).first
+        if user&.authenticate(user_params[:password])
+            save_user(id: user.id)
+            response_template(message: 'Login was successful', status: :ok, data: user)
+        else
+            response_template(message: 'Invalid username/email or password', status: :unauthorized)
+        end
+    end
+
     private
     def user_params
         params.permit(:name, :email, :password)
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,5 @@
 class User < ApplicationRecord
     has_many :reviews
-    has_many :sessions
     has_secure_password
 
     validates :name, {presence: true, uniqueness: true, length: { minimum: 3, maximum: 12 }}
