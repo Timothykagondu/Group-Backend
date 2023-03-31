69718493ae5a9d7b1b1627caed77217995283468
Rukorio-Vanessa <blwwbl55@gmail.com>
2023-03-31 14:33:09 +0300

    added some validations for reviews

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,9 @@
 class Review < ApplicationRecord
   belongs_to :user
   belongs_to :movie
+
+  validates :comment, presence: true, length: { maximum: 500 }
+  validates :user_id, presence: true, numericality: { only_integer: true }
+  validates :movie_id, presence: true, numericality: { only_integer: true }
+
 end
